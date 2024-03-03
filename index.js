f2e93965c616da5699645bbd97e2b82b2d622310
Aya Hassan <147654234+ayaHSE@users.noreply.github.com>
2024-03-03 16:18:40 +0300

    done ;)

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+export function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    const data = {
+      name: name,
+      email: email
+    };
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch(url, options)
+      .then(response => response.json())
+      .then(jsonData => {
+        const id = jsonData.id;
+        document.body.append(id);
+      })
+      .catch(error => {
+        document.body.append(error.message);
+      });
+  }
+  
